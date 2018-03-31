d13d7cce6b4775b6dbf86a5b9991bfa4179b95d2
Dschneids <37875209+Dschneids@users.noreply.github.com>
2018-03-31 16:53:32 -0400

    Create function calls for modifying todo

--- to-dolist/ClientApp/src/app/app.component.ts
+++ to-dolist/ClientApp/src/app/app.component.ts
@@ -9,6 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   toDoList: Array<ToDo> = [];
+
+  addToDo() { }
+
+  editToDo() { }
+
+  completeToDo() { }
 }
 
 interface ToDo {
