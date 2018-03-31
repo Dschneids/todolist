6d5a5464df124a3a1edc301a1b1a5feb9b142f1b
Dschneids <37875209+Dschneids@users.noreply.github.com>
2018-03-31 16:52:12 -0400

    Initial commit/Create ToDo interface

--- to-dolist/ClientApp/src/app/app.component.ts
+++ to-dolist/ClientApp/src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  toDoList: Array<ToDo> = [];
+}
+
+interface ToDo {
+  name: string;
+  completed: boolean;
 }
